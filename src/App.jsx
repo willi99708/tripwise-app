cb606e949d675aa996e5483b54241d3f5134e837
willi99708 <60499272+willi99708@users.noreply.github.com>
2026-06-08 22:04:33 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -308,7 +308,7 @@ function Home({ onSearch, onPickDest, goProfile }) {
     overflow: "hidden",
     backgroundImage: "url('/graphics/main.png')",
     backgroundSize: "cover",
-    backgroundPosition: "center 40%"
+    backgroundPosition: "center -20px"
   }}
 />
       </div>
